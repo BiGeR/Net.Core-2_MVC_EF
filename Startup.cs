4df0be9301b8bedd9555152c57a3c1545cf3b4e7
Brunno <bigercc@gmail.com>
2017-09-15 04:01:55 -0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MeusPedidos_Brunno.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeusPedidos_Brunno
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=C:\Users\BiGeR777\Desktop\Testes_Develing\MeusPedidos_Brunno\MeusPedidosDB.mdf;Trusted_Connection=True;";
+
+            services.AddDbContext<Context>(options => options.UseSqlServer(connection));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
