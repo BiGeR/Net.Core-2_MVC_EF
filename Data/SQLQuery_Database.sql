e03d3279733a9eca55ff9af27bf52c83b6ac8f3d
Brunno <bigercc@gmail.com>
2017-09-15 04:14:55 -0300

    Add files via upload
    Script para gerar um Database simples para testes.

---
+++ Data/SQLQuery_Database.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Candidato] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(50) NOT NULL,
+	[Email] nvarchar(50) NOT NULL,
+	[HTML] int,
+	[Javascript] int,
+	[CSS] int,
+	[Python] int,
+	[Django] int,
+	[Android] int,
+	[Ios]int,
+    CONSTRAINT [PK_Candidato] PRIMARY KEY (Id)
+);
+GO
